3fdf94d90dd406e179ee131b5ee4e8d59eaa2b35
Mateo Aires <mateoaires255@gmail.com>
2025-06-19 19:25:22 -0300

    agregué unas lineas al main para que se pueda conectar al frontend

--- main.py
+++ main.py
@@ -4,10 +4,23 @@ from routers import users
 from routers import auth
 from database import engine, Base
 from routers import orders
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:4200"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # Orígenes permitidos
+    allow_credentials=True,           # Permitir cookies/autenticación
+    allow_methods=["*"],              # Métodos HTTP permitidos
+    allow_headers=["*"],              # Headers permitidos
+)
 
 app.include_router(users.router)
 app.include_router(auth.router)
