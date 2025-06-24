b7fa38ff4e0d65809fc5587c100aa9edcdda76b5
Mateo Aires <mateoaires255@gmail.com>
2025-06-24 10:56:21 -0300

    subo el borrar producto

--- routers/product.py
+++ routers/product.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import SessionLocal
 from models.product import ProductList
 from typing import List
 from dto.product import ProductCreate, ProductResponse
+
 
 router = APIRouter(prefix="/products", tags=["products"])
 
@@ -25,3 +26,23 @@ def create_product(product: ProductCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_product)
     return new_product
+
+@router.put("/{product_id}", response_model=ProductResponse)
+def update_product(product_id: int, product: ProductCreate, db: Session = Depends(get_db)):
+    db_product = db.query(ProductList).filter(ProductList.id == product_id).first()
+    if not db_product:
+        raise HTTPException(status_code=404, detail="Producto no encontrado")
+    for key, value in product.dict().items():
+        setattr(db_product, key, value)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+@router.delete("/{product_id}")
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    db_product = db.query(ProductList).filter(ProductList.id == product_id).first()
+    if not db_product:
+        raise HTTPException(status_code=404, detail="Producto no encontrado")
+    db.delete(db_product)
+    db.commit()
+    return {"mensaje": "Producto eliminado correctamente"}
